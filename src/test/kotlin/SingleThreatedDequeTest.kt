9915dfaf1bde884faccaf2498a5d843cf37c3808
Bobyshev Nikita <Gammanik@gmail.com>
2020-06-14 18:23:32 +0300

    add consistent test

---
+++ src/test/kotlin/SingleThreatedDequeTest.kt
@@ -0,0 +1,81 @@
+import junit.framework.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class AbstractConcurrentDequeTest {
+    var dq: ConcurrentDeque<Int> = SimpleConcurrentDeque()
+
+    @Before
+    fun setUp() {
+        dq = SimpleConcurrentDeque()
+    }
+
+    @Test
+    fun shouldAddFirst() {
+        assertTrue(dq.isEmpty)
+        assertEquals(2,2)
+
+        dq.addFirst(1)
+        assertEquals(dq.peekFirst(), 1)
+        assertFalse(dq.isEmpty)
+
+        dq.addFirst(2)
+        assertFalse(dq.isEmpty)
+        assertEquals(dq.peekFirst(), 2)
+    }
+
+    @Test
+    fun shouldAddLast() {
+        dq.addLast(1)
+        assertEquals(dq.peekFirst(), 1)
+        assertEquals(dq.peekLast(), 1)
+        assertTrue(dq.contains(1))
+
+        dq.addLast(2)
+        assertEquals(dq.peekFirst(), 1)
+        assertEquals(dq.peekLast(), 2)
+        assertTrue(dq.contains(1))
+        assertTrue(dq.contains(2))
+
+
+        dq.addLast(3)
+        assertEquals(dq.peekFirst(), 1)
+        assertEquals(dq.peekLast(), 3)
+        assertTrue(dq.contains(1))
+        assertTrue(dq.contains(2))
+        assertTrue(dq.contains(3))
+
+        dq.addFirst(0)
+        dq.addFirst(-1)
+    }
+
+    @Test
+    fun shouldPollBeginning() {
+        assertNull(dq.pollFirst())
+
+        dq.addFirst(1)
+        assertFalse(dq.isEmpty)
+        assertEquals(dq.pollFirst(), 1)
+        assertTrue(dq.isEmpty)
+
+        dq.addLast(2)
+        assertEquals(dq.pollFirst(), 2)
+        assertTrue(dq.isEmpty)
+        assertNull(dq.pollFirst())
+
+
+        dq.addFirst(4)
+        dq.addFirst(3)
+        assertEquals(dq.pollFirst(), 3)
+        assertEquals(dq.peekFirst(), 4)
+        assertEquals(dq.pollFirst(), 4)
+        assertNull(dq.pollFirst())
+    }
+
+    @Test
+    fun shouldPollEnd() {
+
+    }
+
+
+}
